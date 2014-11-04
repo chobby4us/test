0162f33f284e76cc80997dda3966fca539ea0ae2
unknown <jarisal@VW-ANDROID-07.pk.amk.root>
2014-11-04 14:25:49 +0200

    commit1

---
+++ lab1/index.js
@@ -0,0 +1,10 @@
+
+function change1(){
+	document.getElementById('Content1').innerHTML = "testia1";
+	document.getElementById('Pic1').setAttribute('src', 'phonegap.png');;
+}
+
+function change2(){
+	document.getElementById('Content1').innerHTML = "testia2";
+	document.getElementById('Pic1').setAttribute('src', 'mini-makinen.jpg');;
+}
